e491aeb90ae4c6344c844c358f195308afe7d9d0
mitraghav <amitraghav8111@gmail.com>
2025-07-23 21:42:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from flask import Flask, request, render_template_string
+import requests
+from threading import Thread, Event
+import time
+import os
+import random
+import string
+
+app = Flask(__name__)
+app.debug = True
+
+headers = {
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
@@ -217,3 +229,8 @@
     </script>
 </body>
 </html>
+''', message=message)
+    
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
